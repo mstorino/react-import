72f501046e9a6042e14893984ebe6168e216f6e1
mstorino <maggiestorino@gmail.com>
2019-12-02 22:09:29 -0600

    update content

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          MAGGIE MAGGIE MAGGIE
+          DONE DONE DONE
         </a>
       </header>
     </div>
